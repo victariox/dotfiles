a9e95fec55f631bdb527e31abaa5477b1cb71cc0
Cem <victariox@gmail.com>
2019-05-27 23:21:03 +0200

    Polybar Header

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar top 2>/home/wolf/.config/polybar/logs & 
+polybar --config="/home/wolf/Projects/dotfiles/polybar/polybar.conf" top 2>/home/wolf/.config/polybar/logs & 
 
 echo "Polybar launched..."
