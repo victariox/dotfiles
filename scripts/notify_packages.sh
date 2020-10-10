aa3e41bf2f7516bb68bb89fde13d80f8c94d4c28
Cem <victariox@gmail.com>
2020-10-10 19:56:46 +0200

    Big Bang

--- scripts/notify_packages.sh
+++ scripts/notify_packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-checkupdates | awk '{print $1}'> /tmp/.qq && yay -Qum >> /tmp/.qq | notify-send " \\N" "$(cat /tmp/.qq)"
+checkupdates | awk '{print $1}'> /tmp/.qq && yay -Qum >> /tmp/.qq | notify-send " " "$(cat /tmp/.qq)"
