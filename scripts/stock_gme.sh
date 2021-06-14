469c7546eed4428abcdc2049e59b3f77ef716dd6
M. Cem ISIK <victariox@gmail.com>
2021-06-14 19:44:48 +0200

    Godot Floating Window

--- scripts/stock_gme.sh
+++ scripts/stock_gme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-VAL=`curl  -s 'https://query1.finance.yahoo.com/v8/finance/chart/NOK' | grep -o '"regularMarketPrice":[0-9]*\.[0-9]*' | cut -d':' -f2`
+VAL=`curl  -s 'https://query1.finance.yahoo.com/v8/finance/chart/MT.AS' | grep -o '"regularMarketPrice":[0-9]*\.[0-9]*' | cut -d':' -f2`
 
-echo \$$VAL
+VAL2=`curl  -s 'https://query1.finance.yahoo.com/v8/finance/chart/RKT' | grep -o '"regularMarketPrice":[0-9]*\.[0-9]*' | cut -d':' -f2`
+echo $VAL $VAL2
